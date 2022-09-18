8adffd53094085a701f09577b7717de1c0791ad9
Paul_RazDva <paul1312white@gmail.com>
2022-09-18 11:45:27 +0300

    gitignore

---
+++ src/BonusMilesService.java
@@ -0,0 +1,15 @@
+public class BonusMilesService {
+
+    public int calculate(int cost) {
+        int price = 10_000;
+        int miles = 20 / 1;
+        int calculate = price / miles;
+        return calculate;
+
+    }
+
+
+}
+
+
+
