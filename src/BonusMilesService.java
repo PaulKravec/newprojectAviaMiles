52b6a04235d325b08acbe8014f005a62f235babd
Paul_RazDva <paul1312white@gmail.com>
2022-09-22 14:10:38 +0300

    return

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,10 +1,9 @@
 public class BonusMilesService {
 
     public int calculate(int cost) {
-        int price = 10_000;
-        int miles = 20 / 1;
-        int calculate = price / miles;
-        return calculate;
+        int miles = cost / 20;
+        return miles;
+
 
     }
 
